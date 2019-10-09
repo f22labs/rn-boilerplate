dce3a0e8582090cf30190886f6a77ee133dc7114
Sid <siddharth@f22labs.com>
2019-10-09 14:54:56 +0530

    add react-native-elements header

--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
 import Colors from '../Colors';
+import {Header} from 'react-native-elements';
 
 function FirstScreen({navigation}) {
   return (
     <SafeAreaView
       style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Header />
       <Text
         onPress={() => navigation.navigate('SecondScreen')}
         style={{color: Colors.red}}>
