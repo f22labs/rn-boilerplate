05d6065328647cf3143417559b0ce7ca7968343e
Ganesh <ganesh_r@f22labs.com>
2019-10-09 16:19:37 +0530

    add basic setup for api

---
+++ app/api/config.js
@@ -0,0 +1,8 @@
+import {BASE_API_URL} from 'react-native-dotenv';
+
+const ApiConfig = {
+  baseUrl: BASE_API_URL,
+  urls: {},
+};
+
+export default ApiConfig;
---
+++ app/api/manager.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import {Configs} from '../helpers/Configs';
+// import {UserAuth} from './StoreManager';
+import NavigationService from '../navigation/NavigationService';
+import {BASE_API_URL} from 'react-native-dotenv';
+
+/**
+ * Create an Axios Client with defaults
+ */
+const axiosClient = axios.create({
+  baseURL: BASE_API_URL,
+  timeout: 30000,
+});
+
+/**
+ * Request Wrapper with default success/error actions
+ */
+const Api = async (
+  config = {method: 'GET', data: {}, url: ''},
+  shouldAppendToken = true,
+) => {
+  // Success
+  const onSuccess = response => {
+    console.log('Request Successful!', response.data);
+    return Promise.resolve(response.data);
+  };
+
+  // Error
+  const onError = error => {
+    console.log('Request Failed:', error);
+    if (error.response.status === 401) {
+      /**
+       * Clear the token from offline store
+       * and navigate to Initial Stack
+       */
+    }
+
+    if (error.response) {
+      // Request was made but server responded with something
+      // other than 2xx
+      console.log('Data:', error.response.data);
+      return Promise.reject(error.response.data);
+    } else {
+      // Something else happened while setting up the request
+      // triggered the error
+      console.log('Error Message:', error.message);
+      return Promise.reject(error.message);
+    }
+  };
+
+  // Append headers
+  let headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  // Append auth token
+  // if (shouldAppendToken) {
+  //   try {
+  //     const token = await UserAuth.getToken();
+  //     headers['Access-Token'] = token;
+  //   } catch (error) {
+  //     // Token is not found
+  //     console.log('Error Message:', error.message);
+
+  //     return Promise.reject(error.message);
+  //   }
+  // }
+
+  // Set headers
+  axiosClient.defaults.headers = headers;
+
+  console.log('Request Headers!', axiosClient.defaults.headers);
+  console.log('Request Configurations!', config);
+
+  return axiosClient(config)
+    .then(onSuccess)
+    .catch(onError);
+};
+
+export default Api;
