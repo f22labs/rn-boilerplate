9d30255135613fa8c11d8d0c4e416456d8aca73b
Akhil Mulpuri <akhil@f22labs.com>
2019-10-10 14:48:15 +0530

    added api service

--- app/api/config.js
+++ app/api/config.js
@@ -2,7 +2,12 @@ import {BASE_API_URL} from 'react-native-dotenv';
 
 const ApiConfig = {
   baseUrl: BASE_API_URL,
-  urls: {},
+  urls: {
+    users: {
+      get: '/users/profile',
+      update: '/users/${id}',
+    },
+  },
 };
 
 export default ApiConfig;
---
+++ app/services/UserService.js
@@ -0,0 +1,25 @@
+import API from '../api/manager';
+import config from '../api/config';
+import _ from 'lodash';
+
+const get = () => {
+  return API({
+    method: 'GET',
+    url: config.urls.users.get,
+  });
+};
+const update = (id, data) => {
+  if (!id) throw Error('Id is a required parameter');
+  return API({
+    method: 'PUT',
+    url: _.template(config.urls.users.get)({id}),
+    data,
+  });
+};
+
+export const UserService = {
+  get,
+  post,
+  del,
+  update,
+};
