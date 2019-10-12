001c9751e8cbc5bf43f0d9fe6f8d854f93424718
Ganesh <ganesh_r@f22labs.com>
2019-10-12 13:05:37 +0530

    Formatting fixes

--- app/theme/Colors.js
+++ app/theme/Colors.js
@@ -2,8 +2,8 @@ const Colors = {
   red: 'red',
   alertSuccess: 'rgb(71,177,116)',
   alertError: 'rgb(255,85,85)',
-  overlay: "rgba(0,0,0,0.5)",
-  white: '#ffffff'
+  overlay: 'rgba(0,0,0,0.5)',
+  white: '#ffffff',
 };
 
 export default Colors;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // https://github.com/kmagiera/react-native-gesture-handler/issues/746
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './app/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
