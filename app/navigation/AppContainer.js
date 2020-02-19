bbbf8bc5f484d194e1d5aa9f151e351daaea47a7
vijay <vijayaganth@f22labs.com>
2020-02-19 11:15:42 +0530

    navigation5 added

--- app/navigation/AppContainer.js
+++ app/navigation/AppContainer.js
@@ -1,32 +1,40 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import FirstScreen from '../screens/FirstScreen';
 import SecondScreen from '../screens/SecondScreen';
 import SplashContainer from '../screens/SplashContainer';
+const Stack = createStackNavigator();
 
-const HomeStack = createStackNavigator(
-  {
-    FirstScreen,
-    SecondScreen,
-  },
-  {
-    initialRouteName: 'FirstScreen',
-    defaultNavigationOptions: {
-      header: null,
-    },
-  },
-);
+function Root({ setLoading }) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="splash">
+          {props => <SplashContainer {...props} setLoading={setLoading} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-const RootStack = createSwitchNavigator(
-  {
-    SplashContainer,
-    HomeStack,
-  },
-  {
-    initialRouteName: 'SplashContainer',
-  },
-);
+function AppContainer() {
+  const [loading, setLoading] = React.useState(true);
 
-const AppContainer = createAppContainer(RootStack);
+  if (loading) {
+    return <Root setLoading={setLoading} />;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={FirstScreen} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default AppContainer;
--- app/screens/FirstScreen.js
+++ app/screens/FirstScreen.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Text, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import { Text, View } from 'react-native';
 import Colors from '../theme/Colors';
-// import { ScreenWithHeader } from '../styled_components/StyledCommon';
-import Header from '../components/Header/Header';
-import HeaderCenter from '../components/Header/HeaderCenter';
 import { Image } from 'react-native-elements';
-import HeaderLeft from '../components/Header/HeaderLeft';
-import HeaderRight from '../components/Header/HeaderRight';
-import ScreenWithHeader from '../components/Header/ScreenWithHeader';
 
 function FirstScreen({ navigation }) {
   return (
-    <ScreenWithHeader centerComponent={<HeaderCenter title="Main" />}>
+    <View>
       <Text
         onPress={() => navigation.navigate('SecondScreen')}
         style={{ color: Colors.red }}>
@@ -23,7 +17,7 @@ function FirstScreen({ navigation }) {
           uri: 'https://unsplash.it/200/200?image=1',
         }}
       />
-    </ScreenWithHeader>
+    </View>
   );
 }
 
